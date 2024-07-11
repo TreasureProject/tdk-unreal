eb17a7a915ca20463caa13f776df1f1926b8aabe
MightyRed0907 <mightyred0907@gmail.com>
2024-07-11 15:54:48 +0900

    Commented TDK Json Object

--- Plugins/TDK/Source/TDK/Public/TDKJsonObject.h
+++ Plugins/TDK/Source/TDK/Public/TDKJsonObject.h
@@ -15,22 +15,37 @@ class TDK_API UTDKJsonObject : public UObject
 {
 	GENERATED_UCLASS_BODY()
 
+	/** Create new Json object */
 	UFUNCTION(BlueprintPure, meta = (DisplayName = "Construct Json Object", HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject"), Category = "TDK | Json")
 	static UTDKJsonObject* ConstructJsonObject(UObject* WorldContextObject);
 
+	/** Reset all internal data */
 	UFUNCTION(BlueprintCallable, Category = "TDK | Json")
 	void Reset();
 
+	/** Get the root Json object */
 	TSharedPtr<FJsonObject>& GetRootObject();
 
+	/** Set the root Json object */
 	void SetRootObject(TSharedPtr<FJsonObject>& JsonObject);
 
+
+	//////////////////////////////////////////////////////////////////////////
+	// Serialization
+
+	/** Serialize Json to string */
 	UFUNCTION(BlueprintCallable, Category = "TDK | Json")
 	FString EncodeJson() const;
 
+	/** Construct Json object from string */
 	UFUNCTION(BlueprintCallable, Category = "TDK | Json")
 	bool DecodeJson(const FString& JsonString);
 
+
+	//////////////////////////////////////////////////////////////////////////
+	// Data
+
 private:
+	/** Internal JSON data */
 	TSharedPtr<FJsonObject> JsonObj;
 };
