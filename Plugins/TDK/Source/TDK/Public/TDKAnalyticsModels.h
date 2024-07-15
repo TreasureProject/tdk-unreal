a0bab984d53a3b355233f02698d97a0bdf877097
MightyRed0907 <mightyred0907@gmail.com>
2024-07-15 18:59:47 +0900

    Update Send Event Request properties

--- Plugins/TDK/Source/TDK/Public/TDKAnalyticsModels.h
+++ Plugins/TDK/Source/TDK/Public/TDKAnalyticsModels.h
@@ -15,43 +15,18 @@ struct TDK_API FSendEventRequest : public FTDKRequestCommon
 
 public:
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TDK | Analytics | Event Models")
-    FString SmartAccountAddress;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TDK | Analytics | Event Models")
-    int32 ChainId;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TDK | Analytics | Event Models")
-    FString CartridgeTag;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TDK | Analytics | Event Models")
     FString EventName;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TDK | Analytics | Event Models")
-    FString SessionId;
+    TMap<FString, FString> EventProps;
+};
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TDK | Analytics | Event Models")
-    FString EventId;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TDK | Analytics | Event Models")
-    FString TDKVersion;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TDK | Analytics | Event Models")
-    FString TdkFlavour;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TDK | Analytics | Event Models")
-    int64 EventTimeLocal;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TDK | Analytics | Event Models")
-    int64 EventTimeServer;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TDK | Analytics | Event Models")
-    UTDKJsonObject* EventProps;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TDK | Analytics | Event Models")
-    UTDKJsonObject* DeviceInfo;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TDK | Analytics | Event Models")
-    UTDKJsonObject* AppInfo;
+USTRUCT(BlueprintType)
+struct TDK_API FSendEventResult : public FTDKRequestCommon
+{
+    GENERATED_USTRUCT_BODY()
+public:
+    
 };
 
 USTRUCT(BlueprintType)
