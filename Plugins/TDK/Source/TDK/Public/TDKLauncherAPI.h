f18ef2dde533aba3072ef2dcb03ba74d5bfb39ef
MightyRed0907 <mightyred0907@gmail.com>
2024-09-02 23:15:07 +0900

    Implemented Start Session API in TDK module

--- Plugins/TDK/Source/TDK/Public/TDKLauncherAPI.h
+++ Plugins/TDK/Source/TDK/Public/TDKLauncherAPI.h
@@ -4,20 +4,48 @@
 
 #include "CoreMinimal.h"
 
-#include "Http.h"
-#include "Net/OnlineBlueprintCallProxyBase.h"
+#include "TDKBaseAPI.h"
 
-#include "TDKJsonObject.h"
+#include "TDKLauncherModels.h"
 
 #include "TDKLauncherAPI.generated.h"
 
 class UTDKJsonObject;
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnTDKLauncherRequestCompleted, FTDKBaseModel, Response, bool, Successful);
+
 UCLASS(Blueprintable, BlueprintType)
-class TDK_API UTDKLauncherAPI : public UOnlineBlueprintCallProxyBase
+class TDK_API UTDKLauncherAPI : public UTDKBaseAPI
 {
 	GENERATED_UCLASS_BODY()
 	
-public:
 	
+public:
+	DECLARE_DYNAMIC_DELEGATE_OneParam(FDelegateOnFailureTDKError, FTDKError, Error);
+
+	UPROPERTY(BlueprintAssignable)
+		FOnTDKLauncherRequestCompleted OnTDKResponse;
+
+	/** UOnlineBlueprintCallProxyBase interface */
+	virtual void Activate() override;
+
+
+	//////////////////////////////////////////////////////////////////////////
+	// Generated TDK Launcher API Functions
+	//////////////////////////////////////////////////////////////////////////
+	DECLARE_DYNAMIC_DELEGATE_OneParam(FDelegateOnSuccessStartSession, FStartSessionResult, Result);
+
+	UFUNCTION(BlueprintCallable, Category = "TDK | Launcher | Events ", meta = (BlueprintInternalUseOnly = "true"))
+	static UTDKLauncherAPI* StartSession(FStartSessionRequest Request, FDelegateOnSuccessStartSession OnSuccess,
+		FDelegateOnFailureTDKError OnFailure);
+
+	// Implements FOnTDKLauncherRequestCompleted
+	UFUNCTION(BlueprintCallable, Category = "TDK | Launcher | Authentication ", meta = (BlueprintInternalUseOnly = "true"))
+	void HelperStartSession(FTDKBaseModel Response, bool Successful);
+
+	FDelegateOnFailureTDKError OnFailure;
+	FDelegateOnSuccessStartSession OnSuccessStartSession;
+
+protected:
+	void OnProcessRequestComplete(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful) override;
 };
