64fc736ebec3dbd6210a2a27094d081ccd2b6d90
MightyRed0907 <mightyred0907@gmail.com>
2024-09-02 23:13:46 +0900

    Added model properties for Start Session API in TDK module

--- Plugins/TDK/Source/TDK/Public/TDKLauncherModels.h
+++ Plugins/TDK/Source/TDK/Public/TDKLauncherModels.h
@@ -14,7 +14,20 @@ struct TDK_API FStartSessionRequest : public FTDKRequestCommon
     GENERATED_USTRUCT_BODY()
 
 public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TDK | Launcher | Event Models")
+    FString BackendWallet;
 
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TDK | Launcher | Event Models")
+    TArray<FString> ApprovedTargets;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TDK | Launcher | Event Models")
+    int64 NativeTokenLimitPerTransaction;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TDK | Launcher | Event Models")
+    int32 SessionDurationSec;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TDK | Launcher | Event Models")
+    int32 SessionMinDurationLeftSec;
 };
 
 USTRUCT(BlueprintType)
@@ -23,5 +36,6 @@ struct TDK_API FStartSessionResult : public FTDKRequestCommon
     GENERATED_USTRUCT_BODY()
 
 public:
-
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TDK | Launcher | Event Models")
+    bool Result;
 };
